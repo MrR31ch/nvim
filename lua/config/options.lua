067408cfa46b951eb5ea4948b7126c0a175da6f2
Maximilian Reich <243128438+MReich1@users.noreply.github.com>
2026-02-19 00:17:10 +0100

    4 space tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,6 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 
 -- Set global indentation settings for all file types
-vim.opt.tabstop = 4
+vim.opt.softtabstop = -1
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
